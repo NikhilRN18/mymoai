3197fd8aba91a9a8964145a3ca0164464c905610
NikhilRN18 <107941991+NikhilRN18@users.noreply.github.com>
2024-03-24 10:17:28 -0400

    Update create_fake_people.py

--- python_backend /create_fake_people.py
+++ python_backend /create_fake_people.py
@@ -4,11 +4,10 @@ from faker import Faker
 fake = Faker()
 
 # Load your dataset
-df = pd.read_csv('new_dummy_user_data.csv')  # Replace with your file path
+df = pd.read_csv('new_dummy_user_data.csv')  
 
 # Generate fake names
 df['firstName'] = [fake.first_name() for _ in range(len(df))]
 df['lastName'] = [fake.last_name() for _ in range(len(df))]
 
-# Save the modified DataFrame to a new CSV file
-df.to_csv('people.csv', index=False)  # Replace with your desired new file path
+df.to_csv('people.csv', index=False)  
